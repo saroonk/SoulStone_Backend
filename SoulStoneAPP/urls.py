744c881d24e222b58b8d35e098858ec838db0ae1
saroon <saroonk321@gmail.com>
2026-07-02 13:04:48 +0530

    all page url and views created

--- SoulStoneAPP/urls.py
+++ SoulStoneAPP/urls.py
@@ -7,4 +7,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
+    path('my-orders/', views.my_orders, name='my_orders'),
+    path('order-successful/', views.order_successful, name='order_successful'),
+    path('our-story/', views.ourstory, name='ourstory'),
+    path('product-detail/', views.product_detail, name='product_detail'),
+    path('products/', views.products, name='products'),
+    path('track-order/', views.track_order, name='track_order'),
 ]
--- SoulStoneAPP/views.py
+++ SoulStoneAPP/views.py
@@ -6,3 +6,39 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def my_orders(request):
+    return render(request, 'my-orders.html')
+
+
+def order_successful(request):
+    return render(request, 'order-successful.html')
+
+
+def ourstory(request):
+    return render(request, 'ourstory.html')
+
+
+def product_detail(request):
+    return render(request, 'product-detail.html')
+
+
+def products(request):
+    return render(request, 'product.html')
+
+
+def track_order(request):
+    return render(request, 'track-order.html')
